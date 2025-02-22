57af912ac299e5a7de5320fa1e21826cc2fb925b
sspadwal <sspadwal1002@gmail.com>
2025-02-22 13:51:18 +0530

    Fixed case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
-import MyWork from './components/Mywork/Mywork'
+import MyWork from './components/MyWork/MyWork'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
